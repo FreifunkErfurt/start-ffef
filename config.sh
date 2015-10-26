850ac07831683e228068481bc9d1a2f210385b2d
MKzero <info@linux-web-development.de>
2015-10-26 15:24:45 +0100

    Moved config to shell file

---
+++ config.sh
@@ -0,0 +1,8 @@
+# Site settings
+TITLE="start.ffef"
+DESCRIPTION="Frei von Registrierung, Erfassung von persönlichen Informationen oder zeitlicher Limitierung. Freifunk Erfurt ist ein Netz, an dem jeder teilnehmen kann."
+BASE_URL=""
+ORGANISATION="Freifunk Erfurt"
+TWITTER="FreifunkErfurt"
+GITHUB="freifunkerfurt/start-ffef"
+ABOUT_URL="https://erfurt.freifunk.net/"
